36437764039dcfc9d154709fa01a55131874266b
bencallis1 <bencallis1@gmail.com>
2016-06-30 16:27:00 -0600

    created a private route and an api route

--- routers/server.js
+++ routers/server.js
@@ -4,8 +4,8 @@ var app = express();
 var _ = require('lodash');
 var morgan = require('morgan');
 
-var lionRouter = require('./lions');
-var tigerRouter = require('./tigers');
+var apiRouter = require('./api');
+var privateRouter = require('./private');
 
 app.use(morgan('dev'))
 app.use(express.static('client'));
@@ -14,9 +14,10 @@ app.use(bodyParser.json());
 
 
 // this is called mounting. when ever a req comes in for
-// '/lion' we want to use this router
-app.use('/lions', lionRouter);
-app.use('/tigers', tigerRouter);
+// '/api' we want to use this router
+app.use('/api', apiRouter);
+app.use('/private', privateRouter);
+
 
 app.use(function(err, req, res, next) {
     if (err) {
